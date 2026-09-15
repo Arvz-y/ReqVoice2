69b85ef38d3625afd56bd5726d5b675cb89fc0c0
Arvz-y <darkwarriorsociety98@gmail.com>
2026-09-16 02:23:09 +0800

    Change base path to '/ReqVoice2/'

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  base: '/reqvoice2/',
+  base: '/ReqVoice2/',
   plugins: [
     react(),
     tailwindcss(),
