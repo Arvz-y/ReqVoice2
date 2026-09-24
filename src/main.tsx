3df455a6e5ea4f29cb54fd7251388f88a0ae53f8
Arvz-y <darkwarriorsociety98@gmail.com>
2026-09-24 21:19:21 +0800

    Enable global multilingual UI

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,18 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { ThemeProvider } from './components/ThemeContext.tsx';
+import { LanguageProvider } from './components/LanguageContext.tsx';
+import { TranslationLayer } from './components/TranslationLayer.tsx';
 import { ErrorBoundary } from './components/ErrorBoundary.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ErrorBoundary>
       <ThemeProvider>
-        <App />
+        <LanguageProvider>
+          <App />
+          <TranslationLayer />
+        </LanguageProvider>
       </ThemeProvider>
     </ErrorBoundary>
   </StrictMode>,
